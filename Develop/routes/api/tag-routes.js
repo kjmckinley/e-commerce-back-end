c11ad5a5cb30c451ce9fb931a3f1389cc933e972
Kyle <kj.mckinley@gmail.com>
2021-07-11 00:13:13 -0500

    added ability to update a tag

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -62,6 +62,22 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    },
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
