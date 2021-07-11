0976b7ecd7a4da88783fd7188554b3d16ee85111
Kyle <kj.mckinley@gmail.com>
2021-07-11 00:12:17 -0500

    added ability to create a new tag

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -48,6 +48,16 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    category_name: req.body.category_name
+  })
+    .then(dbCategoryData => {
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
