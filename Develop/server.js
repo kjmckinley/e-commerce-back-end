b5f033bf97f4934a24138ddacb224416515b307f
Kyle <kj.mckinley@gmail.com>
2021-07-15 18:50:47 -0500

    comments in server

--- Develop/server.js
+++ Develop/server.js
@@ -1,17 +1,22 @@
+// for importing an express connection
 const express = require('express');
-const routes = require('./routes');
-// import sequelize connection
+// for importing a sequelize connection
 const sequelize = require('./config/connection');
+// to link and import server routes
+const routes = require('./routes');
 
+// express function
 const app = express();
-const PORT = process.env.PORT || 3001;
+// Port local host  address
+const PORT =  3001;
 
+// server will use these packages and routes
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+// sync the sequelize models to the user database, then turn the server on
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
